0bc77e6ac4a0c97fe3575f910f99823a3249b572
Chen Shu Kai <164017530+Bluebook88@users.noreply.github.com>
2024-12-07 17:53:47 +0800

    v0.2
    完成用戶介面

---
+++ license_system/buttonInit.js
@@ -0,0 +1,15 @@
+function listUserButton() {
+    document.write(`<a class="btn btn-dark m-1" href="userList.html" role="button">List</a>`)
+}
+
+function addUserButton() {
+    document.write(`<a class="btn btn-primary m-1" href="addUser.html" role="button">Add</a>`)
+}
+
+function updateUserButton() {
+    document.write(`<a class="btn btn-info m-1" href="updateUser.html" role="button">Update</a>`)
+}
+
+function deleteUserButton() {
+    document.write(`<a class="btn btn-danger m-1" href="deleteUser.html" role="button">Delete</a>`)
+}
